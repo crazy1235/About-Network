9de03d29b929023a60ae086c7036ea7e6278a66b
jacksen <yansen19920101@gmail.com>
2016-12-26 18:06:16 +0800

    update

---
+++ VolleyDemo/app/src/main/java/com/jacksen/volleydemo/FastJsonRequest.java
@@ -0,0 +1,48 @@
+package com.jacksen.volleydemo;
+
+import com.alibaba.fastjson.JSON;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by Admin on 2016/12/26.
+ */
+
+public class FastJsonRequest<T> extends Request<T> {
+
+    private final Response.Listener<T> listener;
+
+    private Class<T> clazz;
+
+    public FastJsonRequest(int method, String url, Class<T> clazz, Response.Listener<T> listener, Response.ErrorListener errorListener) {
+        super(method, url, errorListener);
+        this.clazz = clazz;
+        this.listener = listener;
+    }
+
+    public FastJsonRequest(String url, Class<T> clazz, Response.Listener<T> listener, Response.ErrorListener errorListener) {
+        this(Method.GET, url, clazz, listener, errorListener);
+    }
+
+    @Override
+    protected Response<T> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+            T t = JSON.parseObject(jsonString, clazz);
+            return Response.success(t, HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return Response.error(new ParseError(e));
+        }
+    }
+
+    @Override
+    protected void deliverResponse(T response) {
+        listener.onResponse(response);
+    }
+}
---
+++ VolleyDemo/app/src/main/java/com/jacksen/volleydemo/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.jacksen.volleydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * home activity
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
