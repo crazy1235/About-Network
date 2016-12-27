8cd51830c1bc0faa6c017e89a421769b4868a3f8
jacksen <yansen19920101@gmail.com>
2016-12-27 15:03:13 +0800

    update

--- BasicNetwork/app/src/main/java/com/jacksen/basicnetwork/SecondActivity.java
+++ BasicNetwork/app/src/main/java/com/jacksen/basicnetwork/SecondActivity.java
@@ -9,11 +9,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
